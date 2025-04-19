0e14dc3f5aec9170dc63a06135c3b2484915cf0a
MaddyMk05 <maadhava05@gmail.com>
2025-04-19 10:40:10 +0530

    Create A-Z is being encoded to numbers.java

---
+++ A-Z is being encoded to numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class main{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int  n=sc.nextInt();
+        if(n>9 && n<=26)
+        {
+            System.out.print(2);
+        }
+        else
+        {
+            System.out.print(1);
+        }
+    }
+}
